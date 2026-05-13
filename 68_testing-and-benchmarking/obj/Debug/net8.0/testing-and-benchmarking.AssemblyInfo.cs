a4bd21ccec8a569a5f0b0bae05fdb8e761f77af4
Jon Marcum <jrmarcum.se@gmail.com>
2026-05-13 18:02:42 -0400

    update

--- 68_testing-and-benchmarking/obj/Debug/net8.0/testing-and-benchmarking.AssemblyInfo.cs
+++ 68_testing-and-benchmarking/obj/Debug/net8.0/testing-and-benchmarking.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testing-and-benchmarking")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1594847c897326d179497d627726ca68d9e6bdb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a250b694d6b885b43cfefeebaf90091d5e0b149d")]
 [assembly: System.Reflection.AssemblyProductAttribute("testing-and-benchmarking")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testing-and-benchmarking")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
